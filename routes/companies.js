b089ced724ed3612d2dabf0f6ba3d805d8b86d36
mlghr <michael.rosenbaum1@gmail.com>
2020-10-02 14:04:12 -0700

    complete, patch route was annoying

--- routes/companies.js
+++ routes/companies.js
@@ -21,59 +21,57 @@ router.get("/", async function (req, res, next) {
 
 router.get('/:code', async (req, res, next) => {
   try {
-    const { code } = req.params;
-    const results = await db.query('SELECT * FROM users WHERE code = $1', [code])
+    const code = req.params.code;
+    const results = await db.query('SELECT code, name, description FROM companies WHERE code = $1', [code]);
     if (results.rows.length === 0) {
       throw new ExpressError(`Can't find user with code of ${code}`, 404)
     }
-    return res.send({ company: results.rows[0] })
+    companyResult = results.rows[0]
+    return res.json({ "company": companyResult })
   } catch (e) {
     return next(e)
   }
 })
   
-  router.get('/search', async (req, res, next) => {
-    try {
-      const { type } = req.query;
-      const results = await db.query(`SELECT * FROM users WHERE type=$1`, [type])
-      return res.json(results.rows)
-    } catch (e) {
-      return next(e)
+  
+router.post('/', async (req, res, next) => {
+  try {
+    const { code, name, description } = req.body;
+    const results = await db.query(`INSERT INTO companies (code, name, description) 
+                                    VALUES ($1, $2, $3) 
+                                    RETURNING code, name, description`, [code, name, description]);                    
+    return res.status(201).json({ "company": results.rows[0] })
+  } catch (e) {
+    return next(e)
+  }
+})
+
+router.patch('/:code', async (req, res, next) => {
+  try {
+    let code = req.params.code;
+    let { name, description } = req.body;
+    const results = await db.query(`UPDATE companies 
+                                    SET name=$1, description=$2 
+                                    WHERE code=$3 RETURNING code, name, description`, 
+                                    [name, description, code]);
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Can't update company with code of ${code}`, 404)
+    } else {
+    return res.json({ "company": results.rows[0] })
     }
-  })
-  
-  router.post('/', async (req, res, next) => {
-    try {
-      const { name, type } = req.body;
-      const results = await db.query('INSERT INTO companies (name, type) VALUES ($1, $2) RETURNING id, name, type', [name, type]);
-      return res.status(201).json({ user: results.rows[0] })
-    } catch (e) {
-      return next(e)
-    }
-  })
-  
-  router.patch('/:id', async (req, res, next) => {
-    try {
-      const { id } = req.params;
-      const { name, type } = req.body;
-      const results = await db.query('UPDATE users SET name=$1, type=$2 WHERE id=$3 RETURNING id, name, type', [name, type, id])
-      if (results.rows.length === 0) {
-        throw new ExpressError(`Can't update user with id of ${id}`, 404)
-      }
-      return res.send({ user: results.rows[0] })
-    } catch (e) {
-      return next(e)
-    }
-  })
-  
-  router.delete('/:id', async (req, res, next) => {
-    try {
-      const results = db.query('DELETE FROM users WHERE id = $1', [req.params.id])
-      return res.send({ msg: "DELETED!" })
-    } catch (e) {
-      return next(e)
-    }
-  })
-  
-  
-  module.exports = router;
+  } catch (e) {
+    return next(e);
+  }
+})
+
+router.delete('/:code', async (req, res, next) => {
+  try {
+    const results = await db.query('DELETE FROM companies WHERE code = $1', [req.params.code])
+    return res.send({ msg: "DELETED!" })
+  } catch (e) {
+    return next(e)
+  }
+})
+
+
+module.exports = router;
--- routes/invoices.js
+++ routes/invoices.js
@@ -5,3 +5,80 @@ const db = require("../db");
 
 let router = new express.Router();
 
+
+router.get("/", async function (req, res, next) {
+    try {
+      const result = await db.query(
+            `SELECT id, comp_code, amt, paid
+             FROM invoices 
+             ORDER BY comp_code`
+      );
+      return res.json({"invoices": result.rows});
+    }
+    catch (err) {
+      return next(err);
+    }
+  });
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const result = await db.query(`SELECT i.comp_code, 
+                                        i.amt, 
+                                        i.paid, 
+                                        i.add_date, 
+                                        i.paid_date
+                                    FROM invoices AS i 
+                                    WHERE id = $1`, [id]);
+    if (result.rows.length === 0) {
+      throw new ExpressError(`Can't find user with code of ${id}`, 404)
+    }
+    invoicesResult = result.rows[0]
+    return res.json({ " Requested invoice": invoicesResult})
+  } catch (e) {
+    return next(e)
+  }
+})
+  
+  
+router.post('/', async (req, res, next) => {
+  try {
+    const { comp_code, amt, paid, add_date } = req.body;
+    const results = await db.query(`INSERT INTO invoices (comp_code, amt, paid, add_date) 
+                                    VALUES ($1, $2, $3, $4) 
+                                    RETURNING id, comp_code, amt, paid, add_date`, [comp_code, amt, paid, add_date]);                    
+    return res.status(201).json({ "inovice": results.rows[0] })
+  } catch (e) {
+    return next(e)
+  }
+})
+
+router.patch('/:id', async (req, res, next) => {
+  try {
+    let id = req.params.id;
+    let { amt, paid } = req.body;
+    const results = await db.query(`UPDATE invoices 
+                                    SET amt=$1, paid=$2 
+                                    WHERE id=$3 RETURNING id, amt, paid, paid_date`, 
+                                    [amt, paid, id]);
+    if (results.rows.length === 0) {
+      throw new ExpressError(`Can't update invoice with id of ${id}`, 404)
+    } else {
+    return res.json({ "invoice": results.rows[0] })
+    }
+  } catch (e) {
+    return next(e);
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const results = await db.query('DELETE FROM invoices WHERE id = $1', [req.params.id])
+    return res.send({ msg: "DELETED!" })
+  } catch (e) {
+    return next(e)
+  }
+})
+
+
+module.exports = router;
